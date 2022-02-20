7b3365c9927429580c53e128a862a1b96a897385
njansari <82147749+njansari@users.noreply.github.com>
2022-02-20 17:08:13 +0000

    Changes

--- Sources/NumberKeyboardTextField/UIColor+Keyboard.swift
+++ Sources/NumberKeyboardTextField/UIColor+Keyboard.swift
@@ -5,7 +5,7 @@ extension UIColor {
         UIColor { traitCollection in
             switch traitCollection.userInterfaceStyle {
             case .dark:
-                return UIColor(displayP3Red: 0.427, green: 0.424, blue: 0.424, alpha: 1)
+                return UIColor(displayP3Red: 0.424, green: 0.424, blue: 0.427, alpha: 1)
             default:
                 return UIColor(displayP3Red: 1, green: 1, blue: 1, alpha: 1)
             }
@@ -18,7 +18,7 @@ extension UIColor {
             case .dark:
                 return UIColor(displayP3Red: 0.282, green: 0.282, blue: 0.282, alpha: 1)
             default:
-                return UIColor(displayP3Red: 0.816, green: 0.761, blue: 0.725, alpha: 1)
+                return UIColor(displayP3Red: 0.725, green: 0.761, blue: 0.816, alpha: 1)
             }
         }
     }
@@ -29,7 +29,7 @@ extension UIColor {
             case .dark:
                 return UIColor(displayP3Red: 0.176, green: 0.176, blue: 0.176, alpha: 1)
             default:
-                return UIColor(displayP3Red: 0.851, green: 0.827, blue: 0.820, alpha: 1)
+                return UIColor(displayP3Red: 0.820, green: 0.827, blue: 0.851, alpha: 1)
             }
         }
     }
